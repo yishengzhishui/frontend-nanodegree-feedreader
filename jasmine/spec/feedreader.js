066006e4d698c92cf93ce0d07442eb24a0dffea2
wangxing <18912540921@163.com>
2018-05-15 21:05:39 +0800

    order

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -67,9 +67,21 @@ $(function() {
       $menuIcon = null;
     });
 
+    /* TODO: Write a test that ensures the menu element is
+     * hidden by default. You'll have to analyze the HTML and
+     * the CSS to determine how we're performing the
+     * hiding/showing of the menu element.
+     */
+
     it(' The menu default is hidden', function() {
       expect($body.hasClass('menu-hidden')).toBeTruthy();
     });
+
+    /* TODO: Write a test that ensures the menu changes
+     * visibility when the menu icon is clicked. This test
+     * should have two expectations: does the menu display when
+     * clicked and does it hide when clicked again.
+     */
 
     it('click menu icon can change state', function() {
       $menuIcon.trigger('click');
@@ -78,6 +90,15 @@ $(function() {
       expect($body.hasClass('menu-hidden')).toBeTruthy();
     });
   });
+
+  /* TODO: Write a new test suite named "Initial Entries" */
+
+  /* TODO: Write a test that ensures when the loadFeed
+   * function is called and completes its work, there is at least
+   * a single .entry element within the .feed container.
+   * Remember, loadFeed() is asynchronous so this test will require
+   * the use of Jasmine's beforeEach and asynchronous done() function.
+   */
 
   describe('Intital Entries', function() {
     beforeEach(function(done) {
@@ -90,6 +111,13 @@ $(function() {
       expect($(".feed .entry").length).not.toBe(0);
     });
   });
+
+  /* TODO: Write a new test suite named "New Feed Selection" */
+
+  /* TODO: Write a test that ensures when a new feed is loaded
+   * by the loadFeed function that the content actually changes.
+   * Remember, loadFeed() is asynchronous.
+   */
 
   describe('New Feed Selection', function() {
     var content1, content2;
@@ -112,31 +140,4 @@ $(function() {
     });
 
   });
-  /* TODO: Write a test that ensures the menu element is
-   * hidden by default. You'll have to analyze the HTML and
-   * the CSS to determine how we're performing the
-   * hiding/showing of the menu element.
-   */
-
-  /* TODO: Write a test that ensures the menu changes
-   * visibility when the menu icon is clicked. This test
-   * should have two expectations: does the menu display when
-   * clicked and does it hide when clicked again.
-   */
-
-  /* TODO: Write a new test suite named "Initial Entries" */
-
-  /* TODO: Write a test that ensures when the loadFeed
-   * function is called and completes its work, there is at least
-   * a single .entry element within the .feed container.
-   * Remember, loadFeed() is asynchronous so this test will require
-   * the use of Jasmine's beforeEach and asynchronous done() function.
-   */
-
-  /* TODO: Write a new test suite named "New Feed Selection" */
-
-  /* TODO: Write a test that ensures when a new feed is loaded
-   * by the loadFeed function that the content actually changes.
-   * Remember, loadFeed() is asynchronous.
-   */
 }());
